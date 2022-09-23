1610b8018c8dccbdf97cfe071f03709e63bf1753
Fabio De Souza Neu <fabio.souza@econvendas.com.br>
2022-09-23 17:04:05 -0300

    Refinamento Projeto

--- scripts/criacao_banco.sql
+++ scripts/criacao_banco.sql
@@ -3,8 +3,8 @@ create database ecommerce;
 use ecommerce;
 
 -- criar tabela cliente
-create table cliente(
-    idcliente int auto_increment primary key,
+create table clients(
+    idClient int auto_increment primary key,
     Fname varchar(20),
     Minit char(3),
     Lname varchar(20),
@@ -14,7 +14,6 @@ create table cliente(
 );
 
 -- cria tabela produto
-
 -- size = dimensão do produto
 create table product(
     idproduct int auto_increment primary key,
@@ -26,10 +25,74 @@ create table product(
     constraint unique_cpf_ unique(CPF)
 );
 
+create table payments(
+ idClient int ,
+ idPayment int,
+ typePayment enum('Boleto','Cartão','Dois cartões'),
+ limitAvailable float,
+ primary key(idClient, id_payment)
+);
+
 -- criar tabela pedido
 create table orders(
     idOrder int auto_increment primary key,
     idOrderClient int,
     orderStatus enum('Cancelado','Confrmado','Em processamento'),
-    order_description varchar(255)
+    order_description varchar(255),
+    sendValue float default 10,
+    payment float,
+    constraint fk_orders_client foreign key( idOrderClient) references clients(idcliente)
+);
+
+-- tabela estoque
+create table productStorage(
+    idProdStorage int auto_increment primary key,
+    StorageLocation varchar(255),
+    quantity int default 0
+);
+
+-- tabela fornecedor
+create table supplier(
+  idSupplier int auto_increment primary key,
+  SocialName varchar(255) not null,
+  CNPJ varchar(15) not null,
+  quantity int default 0,
+  contact char(11) not null,
+  constraint unique_supplier unique(CNPJ)
+);
+
+-- tabela vendedor
+create table seller(
+  idSeller int auto_increment primary key,
+  SocialName varchar(255) not null,
+  abstName varchar(255),
+  CNPJ varchar(15),
+  CPF char(9) ,
+  location varchar(255),
+  contact char(11) not null,
+  constraint unique_cnpj_seller unique(CNPJ),
+  constraint unique_cnpj_seller unique(CPF)
+);
+
+create table productSeller(
+   idPseller int,
+   idProduct int,
+   prodQuantity int default 1,
+   primary key(idSeller,idProduct),
+   constraint fk_product_seller foreign key(idPseller) references seller(idSeller),
+   constraint fk_product_product foreign key(idProduct) references product(idproduct)
+);
+
+create table ClientPJ(
+  idClientPJ int primary key,
+  CNPJ varchar(15),
+  idclient int,
+  constraint fk_clientpj_client foreign key(idclient) references clients(idclient)
+);
+
+create table ClientPF(
+  idClientPF int primary key,
+  CNPJ varchar(15),
+  idclient int,
+  constraint fk_clientpf_client foreign key(idclient) references clients(idclient)
 );
